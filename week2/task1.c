1f82f1da18fdac7d3e720c79c2904dd5d3852cb9
yeon-yeon1 <nojk0106@naver.com>
2025-07-19 04:37:14 +0900

    2주차 문제 정답

---
+++ week2/task1.c
@@ -0,0 +1,41 @@
+/* 
+𝐐1. 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+✔️ 입력 : 첫째 줄에 시험 점수가 주어진다. 시험 점수는 0보다 크거나 같고, 100보다 작거나 같은 정수이다.
+✔️ 출력 : 시험 성적을 출력한다. 
+*/
+
+#include <stdio.h>
+
+int main(void) {
+  int score;
+
+  while (1)
+  {
+    printf("점수를 입력하세요: ");
+    scanf("%d", &score);
+
+    if (score < 0 || score > 100) {
+      printf("점수를 잘못입력하셨습니다.\n");
+    } else {
+        if (score >= 90 && score <= 100){
+          printf("A\n");
+          break;
+        } else if (score >= 80 && score < 90){
+          printf("B\n");
+          break;
+        } else if (score >= 70 && score < 80){
+          printf("C\n");
+          break;
+        } else if (score >= 60 && score < 70){
+          printf("D\n");
+          break;
+        } else {
+          printf("F\n");
+          break;
+        }
+    }
+  }
+  
+  return 0;
+  
+}
---
+++ week2/task6.c
@@ -0,0 +1,38 @@
+/*
+𝐐6. M이상 N이하의 소수를 모두 출력하는 프로그램을 작성하시오. 
+
+✔️ 입력 : 첫째 줄에 자연수 M과 N이 빈 칸을 사이에 두고 주어진다. (1 ≤ M ≤ N ≤ 1,000,000) M이상 N이하의 소수가 하나 이상 있는 입력만 주어진다.
+✔️ 출력 : 한 줄에 하나씩, 증가하는 순서대로 소수를 출력한다.
+*/
+
+#include <stdio.h>
+
+int main(void){
+  int M, N;
+  while (1)
+  {
+    printf("자연수 M,N을 입력하세요: ");
+    scanf("%d %d", &M, &N);
+    if (M > N){
+      printf("다시 입력하세요.\n");
+    } else {
+      for (int i = M; i <= N; i++){
+        if (i < 2) continue;
+
+        int isPrime = 1;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                isPrime = 0;  
+                break;
+            }
+        }
+
+        if (isPrime) {
+            printf("%d\n", i);
+        }
+      }
+      break;
+    }
+  }
+  return 0;
+}
