d5198791c3fd21120144def83f9c609b1de95caf
egaeuni <gaeun3619@gmail.com>
2025-08-01 13:00:46 +0900

    ✨ 1주차 2번 과제

--- week1/task2.c
+++ week1/task2.c
@@ -5,12 +5,11 @@ int main(void){
   int A, B;
   scanf("%d %d", &A, &B);
 
-  printf("%d\n",A+B);
-  printf("%d\n",A-B);
-  printf("%d\n",A*B);
-  // C는 자동 형 변환이 안되므로 정수 나누기 정수의 결과값은 정수로 반환됨
-  printf("%d\n",A/B);
-  printf("%d\n",A%B);
+  printf("%d\n", A+B);
+  printf("%d\n", A-B);
+  printf("%d\n", A*B);
+  printf("%d\n", A/B);
+  printf("%d\n", A%B);
 
   return 0;
 }
